bdd66718219c04fe31074f0eeeb59b65feccba87
amwtke <amwtke@gmail.com>
2022-07-08 10:22:21 +0800

    sched

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4530,7 +4530,7 @@ unsigned long long task_sched_runtime(struct task_struct *p)
 	return ns;
 }
 
-/* xiaojin -- timer interrupt
+/* xiaojin -- timer interrupt scheduler tick  scheduler_tick
 	一个tick就是1ms
 	1ms调一次这个方法 看看哪些进程要need_resched？
  * This function gets called by the timer code, with HZ frequency.
