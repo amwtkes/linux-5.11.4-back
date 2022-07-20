6d8a92bc5e702a09e05990a1acfb2af654339336
amwtke <amwtke@gmail.com>
2022-07-20 15:31:42 +0800

    rcu_note_context_switch

--- tools/testing/selftests/rcutorture/formal/srcu-cbmc/src/preempt.h
+++ tools/testing/selftests/rcutorture/formal/srcu-cbmc/src/preempt.h
@@ -35,7 +35,7 @@ static inline int preempt_count(void)
 	return preempt_disable_count;
 }
 
-static inline bool rcu_note_context_switchpreemptible(void)
+static inline bool preemptible(void)
 {
 	return !preempt_count();
 }
