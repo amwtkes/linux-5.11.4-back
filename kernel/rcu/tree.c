293c063c11aa7f4befa45aa4a9b5ba0a520b3903
amwtke <amwtke@gmail.com>
2022-07-29 16:53:28 +0800

    some comments

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -628,7 +628,16 @@ EXPORT_SYMBOL_GPL(rcutorture_get_gp_data);
  * the possibility of usermode upcalls having messed up our count
  * of interrupt nesting level during the prior busy period.
  */
-/*xiaojin-rcu eqs enter extended QS的处理！*/
+/*xiaojin-rcu rcu_qs Extended-QS的处理！
+do_idle
+  ...
+    ->rcu_idle_enter
+      ->rcu_eqs_enter
+	  
+为什么要有EQS呢？主要是为了动态时钟的场景，若系统开启了NO_HZ_IDLE，在运行idle进程的情况下CPU是不响应tick中断的
+若开启NO_HZ_FULL，不仅CPU Idle时不响应tick，在CPU只有一个running状态的进程时也不响应tick中断。前面我们讲tick中断时已经说到它的功能包括检查QS，以及唤醒RCU软中断。那在没有的tick的情况下CPU如何上报QS呢？
+看上面的函数分析，进入EQS时会调用rcu_eqs_enter()->rcu_dynticks_eqs_enter()将当前CPU的rdp->dynticks的bit1加1变为奇数，表示处于动态时钟模式，而gp线程在处理强制静止态force qs的操作中会搜集所有的处于EQS的CPU，并替它们上报QS。
+既然有进入，那就会有退出，退出EQS模式时会将rdp->dynticks的bit1减1，变为偶数。对应函数rcu_eqs_exit() ，场景包括退出idle进程、进入中断。*/
 static noinstr void rcu_eqs_enter(bool user)
 {
 	struct rcu_data *rdp = this_cpu_ptr(&rcu_data);
