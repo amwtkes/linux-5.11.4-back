2c526aabc1d3035dd02981b6812ec639e46899a3
amwtke <amwtke@gmail.com>
2022-10-25 17:50:24 +0800

    arm spinlock begin

--- arch/arm/include/asm/spinlock_types.h
+++ arch/arm/include/asm/spinlock_types.h
@@ -8,7 +8,7 @@
 
 #define TICKET_SHIFT	16
 
-/*xiaojin-spinlock 2.2 一般情况 arch_spinlock_t同样也是32位的int，但是用了ticket算法来排队，所以更公平。
+/*xiaojin-spinlock 2.2-arm-lock arm平台使用ticket算法来做，类似于叫号的算法。
 https://app.yinxiang.com/shard/s65/nl/15273355/e6b21781-3aab-4f5a-b53c-fc716c6dbf0f/
 */
 typedef struct {
